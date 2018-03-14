e4b7a91a6654ee5fd98434b131abcf02aaf38da0
Julian T Skodzinski <juliantsk@gmail.com>
2018-03-14 07:05:18 -0400

    Initiated all the main files.

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyA1-Ecyp2NlHasTNKHhoPjtGyD_FQztno4",
+    authDomain: "hello-f8ef5.firebaseapp.com",
+    databaseURL: "https://hello-f8ef5.firebaseio.com",
+    projectId: "hello-f8ef5",
+    storageBucket: "hello-f8ef5.appspot.com",
+    messagingSenderId: "22984384033"
+};
+firebase.initializeApp(config);
